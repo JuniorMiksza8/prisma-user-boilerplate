0581bf390a7bd0a4866aec4efb7e976877038e55
Junior Miksza <junior.miksza8@gmail.com>
2022-01-26 19:30:38 -0300

    switch from pure express to routing controllers

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,9 @@
 import 'express-async-errors'
-import express, { NextFunction, Request, Response } from 'express'
-import routes from './routes'
+import 'reflect-metadata'
+import { createExpressServer } from 'routing-controllers'
 
-const app = express()
-
-app.use(express.json())
-
-app.use(routes)
-
-app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
-    return response.status(500).json({
-        message: error.message,
-    })
+const app = createExpressServer({
+    controllers: [__dirname + '/domain/*/*.controller.{ts,js}'],
 })
 
 export default app
--- src/domain/authentication/auth.controller.ts
+++ src/domain/authentication/auth.controller.ts
@@ -1,53 +1,56 @@
 import { compare } from 'bcrypt'
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 import { signUserToken } from '../../services/jwt'
 import { PostSigninDTO } from './auth.dto'
 import { findRefreshToken, findUserByID, findUserByUsername } from './auth.service'
 import { omit } from 'lodash'
-const signInRouter = Router()
+import { Body, JsonController, Post, Req, Res } from 'routing-controllers'
 
-signInRouter.route('/signin').post(async (req, res) => {
-    const { username, password } = req.body as PostSigninDTO
+@JsonController('/auth')
+export class AuthController {
+    @Post('/signin')
+    async SignIn(@Body() body: PostSigninDTO, @Res() res: Response) {
+        const { username, password } = body
 
-    const user = await findUserByUsername(username)
+        const user = await findUserByUsername(username)
 
-    if (!user) {
-        return res.status(404).json({
-            message: 'username or password invalid',
+        if (!user) {
+            return res.status(404).json({
+                message: 'username or password invalid',
+            })
+        }
+
+        const isValid = await compare(password, user.password)
+
+        if (!isValid) {
+            return res.status(404).json({
+                message: 'username or password invalid',
+            })
+        }
+
+        const accessToken = signUserToken(user.id)
+
+        return res.json({
+            accessToken,
+            user: omit(user, ['password', 'salt']),
         })
     }
 
-    const isValid = await compare(password, user.password)
+    @Post('/renew')
+    async renew(@Req() req: Request, @Res() res: Response) {
+        const { refreshToken } = req.body
 
-    if (!isValid) {
-        return res.status(404).json({
-            message: 'username or password invalid',
+        const token = await findRefreshToken(refreshToken)
+
+        if (!token) {
+            return res.status(404).json({ message: 'token not found' })
+        }
+
+        const user = await findUserByID(token.userId)
+
+        return res.json({
+            user,
+            refreshToken: token,
         })
     }
-
-    const accessToken = signUserToken(user.id)
-
-    return res.json({
-        accessToken,
-        user: omit(user, ['password', 'salt']),
-    })
-})
-
-signInRouter.route('/renew').post(async (req, res) => {
-    const { refreshToken } = req.body
-
-    const token = await findRefreshToken(refreshToken)
-
-    if (!token) {
-        return res.status(404).json({ message: 'token not found' })
-    }
-
-    const user = await findUserByID(token.userId)
-
-    return res.json({
-        user,
-        refreshToken: token,
-    })
-})
-
-export default signInRouter
+}
